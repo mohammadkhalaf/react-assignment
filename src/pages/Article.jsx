0af04778af9ea8959d6e2100cddf9c5b5fd34c74
mohammad <mohammadkhalaf@live.com>
2023-11-10 13:22:06 +0100

    style article page

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,95 +1,128 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import ArticlesWrapper from '../components/articleComponents/ArticlesWrapper';
-import Blog from '../components/Blog';
-import Footer from '../components/Footer';
-import {FiSearch} from 'react-icons/fi'
-const recentArticles=[
-    {title:'How to Blow Through Capital At an Incredible Rate', published:'Jan 14, 2020'},
-    {title:'Design Studios That everyone should know about', published:'Jan 14, 2020'},
-    {title:'How did we get 1m+ visitors in 30 days without anything', published:'Jan 14, 2020'},
-    {title:'Figma on figma:How we built our website design system', published:'Jan 14, 2020'}
-]
-const categories=[
-    {title:'Technology', numOfPosts:'20 posts'},  {title:'Freelancing', numOfPosts:'7 posts'},  {title:'Writing', numOfPosts:'16 posts'},  {title:'Marketing', numOfPosts:'11 posts'},  {title:'Business', numOfPosts:'35 posts'},  {title:'Education', numOfPosts:'14 posts'}
-]
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ArticlesWrapper from "../components/articleComponents/ArticlesWrapper";
+import Blog from "../components/Blog";
+import Footer from "../components/Footer";
+import { FiSearch } from "react-icons/fi";
+const recentArticles = [
+  {
+    title: "How to Blow Through Capital At an Incredible Rate",
+    published: "Jan 14, 2020",
+  },
+  {
+    title: "Design Studios That everyone should know about",
+    published: "Jan 14, 2020",
+  },
+  {
+    title: "How did we get 1m+ visitors in 30 days without anything",
+    published: "Jan 14, 2020",
+  },
+  {
+    title: "Figma on figma:How we built our website design system",
+    published: "Jan 14, 2020",
+  },
+];
+const categories = [
+  { title: "Technology", numOfPosts: "20 posts" },
+  { title: "Freelancing", numOfPosts: "7 posts" },
+  { title: "Writing", numOfPosts: "16 posts" },
+  { title: "Marketing", numOfPosts: "11 posts" },
+  { title: "Business", numOfPosts: "35 posts" },
+  { title: "Education", numOfPosts: "14 posts" },
+];
 
 const Article = () => {
-    const [article,setArticle]=useState()
-    const [loading,setLoading]=useState(false)
-    const date= new Date(article?.published)
-    const day = date.getDate(); 
-    const year= date.getFullYear()
-    const month = date.toLocaleString('default', { month: 'short' });
-    const {id}= useParams()
-    console.log(id);
-    useEffect(()=>{
-        const getArticle=async()=>{
-            try {
-                setLoading(true)
-                const response =await axios.get(`https://win23-assignment.azurewebsites.net/api/articles/${id}`)
-                const {data}=response
-                if(response.status===200){
-                    setArticle(data)
-                }
-               
-                
-            } catch (error) {
-                
-            }
-             setLoading(false)
-
+  const [article, setArticle] = useState();
+  const [loading, setLoading] = useState(false);
+  const date = new Date(article?.published);
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const month = date.toLocaleString("default", { month: "short" });
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    const getArticle = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://win23-assignment.azurewebsites.net/api/articles/${id}`
+        );
+        const { data } = response;
+        if (response.status === 200) {
+          setArticle(data);
         }
-        getArticle()
+      } catch (error) {}
+      setLoading(false);
+    };
+    getArticle();
+  }, []);
+  console.log(article);
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
 
-    },[])
-    console.log(article);
-    if(loading){
-        return <h1>loading...</h1>
-    }
- 
-  return <>
-           <section className='section-container article-container'>
-           {article && <>
-             <div className='article-right'>
-             <h1>{article.title}</h1>
-             <p>{month} {day}, {year} * {article.category} * {article.author}</p> 
-              <div>
-                  
-              <div className="img-container" style={{maxWidth:'750px'}}>
-                    <img style={{width:'100%'}} src={article.imageUrl} alt="" />
+  return (
+    <>
+      <section className="section-container ">
+        {article && (
+          <>
+            <div className="article-container">
+              <div className="article-left">
+                <h1>{article.title}</h1>
+                <p className="subtitle">
+                  {month} {day}, {year} <span></span> {article.category}
+                  <span></span> {article.author}
+                </p>
+                <div>
+                  <div className="img-container" style={{ maxWidth: "750px" }}>
+                    <img
+                      style={{ width: "100%" }}
+                      src={article.imageUrl}
+                      alt=""
+                    />
+                  </div>
+                  <p className="article-content">{article.content}</p>
                 </div>
-                <p>{article.content}</p>
-            </div>
-             </div>
-            <div className='article-left'>
-                <div className='search-input'>
-                <input type="text"  placeholder='Type to search'/>
-                <FiSearch className='search-icon'/>
+              </div>
+              <div className="article-right">
+                <div className="search-input">
+                  <input type="text" placeholder="Type to search..." />
+                  <FiSearch className="search-icon" />
                 </div>
                 <div className="recent-articles">
-                    <h3>Recent Posts</h3>
-                    <ul>
-                        {recentArticles.map((article)=>{
-                            return <li> {article.title} <span>{article.published}</span></li>
-                        })}
-                    </ul>
+                  <h3>Recent Posts</h3>
+                  <ul>
+                    {recentArticles.map((article) => {
+                      return (
+                        <li className="recent-articles-item">
+                          {article.title} <span>{article.published}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
                 <div className="categories">
-                    <h3>Categories</h3>
-                    <ul>
-                        {categories.map((category)=>{
-                            return <li> {category.title} <span>{category.numOfPosts}</span></li>
-                        })}
-                    </ul>
+                  <h3>Categories</h3>
+                  <ul>
+                    {categories.map((category) => {
+                      return (
+                        <li className="category-item">
+                          {category.title} - <span>{category.numOfPosts}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
+              </div>
             </div>
-           </> }
-           </section>
-           <Blog/>
-           <Footer/>
-        </>
+          </>
+        )}
+      </section>
+      <Blog />
+      <Footer />
+    </>
+  );
 };
 
 export default Article;
